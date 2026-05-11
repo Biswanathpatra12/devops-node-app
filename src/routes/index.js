e58565c3ea4872e0b368e379e323213843847d0f
Biswanath Patra <biswanathpatra@Biswanaths-MacBook-Air.local>
2026-05-11 16:59:13 +0530

    final esm fix all imports

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
-import session from './session';
-import user from './user';
-import message from './message';
+import session from './session.js';
+import user from './user.js';
+import message from './message.js';
 
-export default {
+export {
   session,
   user,
   message,
